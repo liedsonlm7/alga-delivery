804bca1b22c170f475d542d158f45fd3d6aa8133
liedsonlm7 <liedsonlm7@gmail.com>
2026-01-14 14:49:31 -0300

    (feat): create domain entities

---
+++ Microservices/delivery-tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/ContactPoint.java
@@ -0,0 +1,17 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+public class ContactPoint {
+    private String zipCode;
+    private String street;
+    private String number;
+    private String complement;
+    private String name;
+    private String phone;
+}
---
+++ Microservices/delivery-tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,82 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.*;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class Delivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private UUID courierId;
+
+    private DeliveryStatus status;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveryAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFee;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCost;
+
+    private Integer totalItems;
+
+    private ContactPoint sender;
+    private ContactPoint recipient;
+
+    private List<Item> items = new ArrayList<>();
+
+    public static Delivery draft() {
+        Delivery delivery = new Delivery();
+        delivery.setId(UUID.randomUUID());
+        delivery.setStatus(DeliveryStatus.DRAFT);
+        delivery.setTotalItems(0);
+        delivery.setTotalCost(BigDecimal.ZERO);
+        delivery.setCourierPayout(BigDecimal.ZERO);
+        delivery.setDistanceFee(BigDecimal.ZERO);
+
+        return delivery;
+    }
+
+    public UUID addItem(String name, int quantity) {
+        Item item = Item.brandNew(name, quantity);
+        items.add(item);
+        calculateTotalItems();
+        return item.getId();
+    }
+
+    public void removeItem(UUID itemId) {
+        items.removeIf(item -> item.getId().equals(itemId));
+        calculateTotalItems();
+    }
+
+    public void changeItemQuantity(UUID itemId, int quantity) {
+        Item item = getItems().stream().filter(i -> i.getId().equals(itemId))
+                .findFirst().orElseThrow();
+
+        item.setQuantity(quantity);
+        calculateTotalItems();
+    }
+
+    public void removeItems() {
+        items.clear();
+        calculateTotalItems();
+    }
+
+    public List<Item> getItems() {
+        return Collections.unmodifiableList(this.items);
+    }
+
+    private void calculateTotalItems() {
+        int totalItems = getItems().stream().mapToInt(Item::getQuantity).sum();
+        setTotalItems(totalItems);
+    }
+}
